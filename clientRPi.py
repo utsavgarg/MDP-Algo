28d87d9d5a57cde54da04b8ebe9d64d496f5cb0e
Marcel <marc0036@e.ntu.edu.sg>
2017-09-26 11:19:19 +0800

    Rewritten the write and receive function

--- clientRPi.py
+++ clientRPi.py
@@ -17,111 +17,46 @@ from Algo.Exploration import Exploration
 from Algo.FastestPath import FastestPath
 from Algo.Constants import START, GOAL, NORTH
 
-# class WFConnector:
-
-# host = '192.168.26.1'
-# port = 8008
-#
-# s = None
-# for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
-#     family, socket_type, proto, canonname, socket_address = res
-#     try:
-#         s = socket.socket(family, socket_type, proto)
-#     except socket.error:
-#         s = None
-#         continue
-#     try:
-#         s.connect(socket_address)
-#     except socket.error:
-#         s.close()
-#         s = None
-#         continue
-#     break
-# if s is None:
-#     print('could not open socket')
-#
-# while 1:
-#
-#     dataToBeSent = str.encode(input("Input string: "))
-#     s.sendall(dataToBeSent)
-#
-#     data = s.recv(1024)
-#     print('Received', repr(data))
-# s.close()
-
-
-class Connector:
-    def __init__(self):
-
-        family = socket.AF_INET
-        socket_type = socket.SOCK_STREAM
-        self.socket = socket.socket(family, socket_type)
-        self.socket.settimeout(1)
-        self.connected = False
-        self.connect()
-
-    def connect(self):
-
-        # for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
-        #     family, socket_type, proto, canonname, socket_address = res
-        #     try:
-        #         s = socket.socket(family, socket_type, proto)
-        #     except socket.error:
-        #         continue
-        #     try:
-        #         s.connect(socket_address)
-        #     except socket.error:
-        #         s.close()
-        #         continue
-        #     s.settimeout(0.5)
-        #     # s.setblocking(False)
-        #     self.socket = s
-        #     print("[Info] Connection established.")
-        #     print("         family: ", family)
-        #     print("         socket_type: ", socket_type)
-        #     print("         proto: ", proto)
-        #     break
-        host = '192.168.26.1'
-        port = 8008
-        try:
-            self.socket.connect((host, port))
-        except Exception:
-            print("[Error] Unable to establish connection.")
-        else:
-            self.connected = True
-            print("[Info] Connection established.")
-
-    def send(self, msg):
-        if not self.connected:
-            self.connect()
-        if self.connected:
-            print("[Info] Sending message: ", msg)
-            try:
-                self.socket.sendall(str.encode(msg))
-            except Exception:
-                print("[Error] Unable to send message. Connection loss.")
-                # self.connected = False
-
-    def receive(self):
-        if not self.connected:
-            self.connect()
-        if self.connected:
-            try:
-                msg = self.socket.recv(1024)
-                if msg:
-                    print("[Info] Received: ", msg.decode())
-                return msg
-            except socket.timeout:
-                print("No message is received.")
-        # else:
-        #     print("[Error] Unable to receive message. Connection loss.")
-
-
-connector = Connector()
-while True:
-    dataToBeSent = input("Input string: ")
-    connector.send(dataToBeSent)
-    connector.receive()
+class Test(threading.Thread):
+        def __init__(self):
+                threading.Thread.__init__(self)
+               
+                self.ip = "192.168.26.1" # Connecting to IP address of MDPGrp26
+                self.port = 5182
+               
+                # Create a TCP/IP socket
+                self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client_socket.connect((self.ip, self.port))
+ 
+        # Send data
+        def write(self, count = 0):
+                print "\nEnter text to send: "
+                msg = raw_input()
+                while True:
+                        # time.sleep(1)
+                        self.client_socket.send(msg)
+                        print "\nEnter text to send: "
+                        msg = raw_input()
+                       # msg ="HA90|"
+                        count += 1
+                print "quit write()"
+ 
+        # Receive data
+        def receive(self):
+                while True:
+                        data = self.client_socket.recv(1024)
+                        if len(data) == 0:
+                                print "Quitting..."
+                                break
+                        print "\nFrom RPI: %s " % data
+                print "Quit receive()"
+       
+        def keep_main(self):
+                while True:
+                        time.sleep(0.5)
+                        
+                        
+                        
 
 
 define("port", default=8888, help="run on the given port", type=int)
